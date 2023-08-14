01279cbf9c877c41dab5ca5c15f71816d3ac17d2
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-14 10:41:25 -0500

    Create Todo JPA Entity

---
+++ todo-management-backend/src/main/java/net/fernandosalas/todo/entity/Todo.java
@@ -0,0 +1,26 @@
+package net.fernandosalas.todo.entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    private boolean completed;
+}
