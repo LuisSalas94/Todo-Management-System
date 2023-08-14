4ecf9bb4daed18e0b0a9ccd352ac6d9c959a548e
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-14 10:27:13 -0500

    Configure MySQL Database

---
+++ todo-management-backend/src/main/java/net/fernandosalas/todo/TodoManagementApplication.java
@@ -0,0 +1,13 @@
+package net.fernandosalas.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementApplication.class, args);
+	}
+
+}
---
+++ todo-management-backend/src/test/java/net/fernandosalas/todo/TodoManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fernandosalas.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
