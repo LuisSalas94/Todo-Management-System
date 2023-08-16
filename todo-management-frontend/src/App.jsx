074377893f1777361909b817f20189e4e2a4e984
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-16 02:05:55 -0500

    Add Header Component

--- todo-management-frontend/src/App.jsx
+++ todo-management-frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import HeaderComponent from "./components/HeaderComponent";
+
 import ListTodoComponent from "./components/ListTodoComponent";
 
 function App() {
   return (
     <>
+      <HeaderComponent />
       <ListTodoComponent />
     </>
   );
---
+++ todo-management-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,17 @@
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div>
+            <a className="navbar-brand" href="#">
+              Todo Management System
+            </a>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderComponent;
