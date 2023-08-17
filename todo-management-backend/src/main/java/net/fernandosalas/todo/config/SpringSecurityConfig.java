e4043aec829b4fd8f3d8d139e4ff3aa4fd90e252
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-17 04:53:10 -0500

    Database Auth

--- todo-management-backend/src/main/java/net/fernandosalas/todo/config/SpringSecurityConfig.java
+++ todo-management-backend/src/main/java/net/fernandosalas/todo/config/SpringSecurityConfig.java
@@ -1,9 +1,13 @@
 package net.fernandosalas.todo.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,7 +21,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
+@AllArgsConstructor
 public class SpringSecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -40,21 +49,30 @@ public class SpringSecurityConfig {
         return http.build();
     }
 
+    // Database Auth
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails fernando = User.builder()
-                .username("fernando")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(fernando, admin);
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
     }
+
+
+    // In Memory Auth
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails fernando = User.builder()
+//                .username("fernando")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("USER")
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("admin"))
+//                .roles("ADMIN")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(fernando, admin);
+//    }
 
 }
---
+++ todo-management-backend/src/main/java/net/fernandosalas/todo/utils/PasswordEncoderImpl.java
@@ -0,0 +1,12 @@
+package net.fernandosalas.todo.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderImpl {
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("fernando"));
+        System.out.println(passwordEncoder.encode("admin"));
+    }
+}
