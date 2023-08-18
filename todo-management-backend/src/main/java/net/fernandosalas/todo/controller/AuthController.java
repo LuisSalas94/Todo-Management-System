ab7ae9e8c1083c9aa631c5729883a1428485ccc3
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-18 07:15:31 -0500

    Connect Frontend to Register REST API

--- todo-management-backend/src/main/java/net/fernandosalas/todo/controller/AuthController.java
+++ todo-management-backend/src/main/java/net/fernandosalas/todo/controller/AuthController.java
@@ -7,11 +7,9 @@ import net.fernandosalas.todo.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/auth")
--- todo-management-frontend/src/components/RegisterComponent.jsx
+++ todo-management-frontend/src/components/RegisterComponent.jsx
@@ -1,5 +1,105 @@
+import { useState } from "react";
+import { registerAPICall } from "../services/AuthService";
+
 const RegisterComponent = () => {
-  return <div>RegisterComponent</div>;
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+    const register = {
+      name,
+      username,
+      email,
+      password,
+    };
+    try {
+      await registerAPICall(register);
+      console.log("Successfully Registered");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-header">
+              <h2 className="text-center">User Registration Form</h2>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="row mb-3">
+                  <label className="col-md-3 control-label">Name</label>
+                  <div className="col-md-9">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="name"
+                      placeholder="Enter Name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row mb-3">
+                  <label className="col-md-3 control-label">Username</label>
+                  <div className="col-md-9">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="username"
+                      placeholder="Enter Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row mb-3">
+                  <label className="col-md-3 control-label">Email</label>
+                  <div className="col-md-9">
+                    <input
+                      type="email"
+                      className="form-control"
+                      name="email"
+                      placeholder="Enter Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="row mb-3">
+                  <label className="col-md-3 control-label">Password</label>
+                  <div className="col-md-9">
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Enter Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="form-group mb-3">
+                  <button
+                    className="btn btn-outline-primary"
+                    onClick={(e) => handleRegistration(e)}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterComponent;
