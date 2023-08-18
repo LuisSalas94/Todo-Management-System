3eae5421e0b38e6f06591ec6e917063e83c75a2b
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-18 02:18:42 -0500

    Build Login REST API

--- todo-management-backend/src/main/java/net/fernandosalas/todo/controller/AuthController.java
+++ todo-management-backend/src/main/java/net/fernandosalas/todo/controller/AuthController.java
@@ -1,6 +1,7 @@
 package net.fernandosalas.todo.controller;
 
 import lombok.AllArgsConstructor;
+import net.fernandosalas.todo.dto.LoginDto;
 import net.fernandosalas.todo.dto.RegisterDto;
 import net.fernandosalas.todo.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,18 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    // Register REST API
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
         String response = authService.register(registerDto);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    // Login REST API
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        String response = authService.login(loginDto);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
--- todo-management-backend/src/main/java/net/fernandosalas/todo/service/AuthService.java
+++ todo-management-backend/src/main/java/net/fernandosalas/todo/service/AuthService.java
@@ -1,7 +1,8 @@
 package net.fernandosalas.todo.service;
-
+import net.fernandosalas.todo.dto.LoginDto;
 import net.fernandosalas.todo.dto.RegisterDto;
 
 public interface AuthService {
     String register(RegisterDto registerDto);
+    String login(LoginDto loginDto);
 }
