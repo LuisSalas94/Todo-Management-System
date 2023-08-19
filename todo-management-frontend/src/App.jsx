ba34bfcab23d9dd1ab4e28c25dd9b93bccc7bf84
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-19 10:28:16 -0500

    Secure the Routes

--- todo-management-frontend/src/App.jsx
+++ todo-management-frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
 import ListTodoComponent from "./components/ListTodoComponent";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import TodoComponent from "./components/TodoComponent";
 import RegisterComponent from "./components/RegisterComponent";
 import LoginComponent from "./components/LoginComponent";
+import { isUserLoggedIn } from "./services/AuthService";
 
 function App() {
+  const AuthenticatedRoute = ({ children }) => {
+    const isAuth = isUserLoggedIn();
+    if (isAuth) {
+      return children;
+    }
+    return <Navigate to="/" />;
+  };
+
   return (
     <>
       <BrowserRouter>
@@ -15,9 +24,31 @@ function App() {
           {/* <Route path="/" element={<ListTodoComponent />} /> */}
           {/* Diplay the Links as Per User Auth in the Header */}
           <Route path="/" element={<LoginComponent />} />
-          <Route path="/todos" element={<ListTodoComponent />} />
-          <Route path="/add-todo" element={<TodoComponent />} />
-          <Route path="/update-todo/:id" element={<TodoComponent />} />
+          {/* Secure the Routes */}
+          <Route
+            path="/todos"
+            element={
+              <AuthenticatedRoute>
+                <ListTodoComponent />
+              </AuthenticatedRoute>
+            }
+          />
+          <Route
+            path="/add-todo"
+            element={
+              <AuthenticatedRoute>
+                <TodoComponent />
+              </AuthenticatedRoute>
+            }
+          />
+          <Route
+            path="/update-todo/:id"
+            element={
+              <AuthenticatedRoute>
+                <TodoComponent />
+              </AuthenticatedRoute>
+            }
+          />
           <Route path="/register" element={<RegisterComponent />} />
           <Route path="/login" element={<LoginComponent />} />
         </Routes>
