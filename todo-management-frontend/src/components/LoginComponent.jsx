c042e9db8b3a766978b71996fdb7f0bd1f63898c
Fernando Salas <luisfernandosalasg@gmail.com>
2023-08-19 07:07:56 -0500

    Design Login Form

--- todo-management-frontend/src/components/LoginComponent.jsx
+++ todo-management-frontend/src/components/LoginComponent.jsx
@@ -1,5 +1,73 @@
+import { useState } from "react";
+
 const LoginComponent = () => {
-  return <div>LoginComponent</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginForm = (e) => {
+    e.preventDefault();
+    const loginObj = {
+      username,
+      password,
+    };
+
+    console.log(loginObj);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-header">
+              <h2 className="text-center">Login Form</h2>
+            </div>
+            <div className="card-body">
+              <form>
+                <div className="row mb-3">
+                  <label className="col-md-3 control-label">
+                    Username or Email
+                  </label>
+                  <div className="col-md-9">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="username"
+                      placeholder="Enter Username"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                    />
+                  </div>
+                </div>
+
+                <div className="row mb-3">
+                  <label className="col-md-3 control-label">Password</label>
+                  <div className="col-md-9">
+                    <input
+                      type="password"
+                      className="form-control"
+                      name="password"
+                      placeholder="Enter Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="form-group mb-3">
+                  <button
+                    className="btn btn-outline-primary"
+                    onClick={(e) => handleLoginForm(e)}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginComponent;
